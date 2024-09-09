4db397b5e1b377f0c2142d3465c9a42377ed48ac
Nick <zubkov.kol@yandex.ru>
2024-09-09 15:35:46 +0300

    fix #1

--- vars/TelegramNotifier.groovy
+++ vars/TelegramNotifier.groovy
@@ -1,7 +1,8 @@
 def send(String message) {
-	def token = 'env.TELEGRAM_BOT_TOKEN'
-	def chatId = 'env.TELEGRAM_CHAT_ID'
+	def token = env.TELEGRAM_BOT_TOKEN
+	def chatId = env.TELEGRAM_CHAT_ID
 	def encodedMessage = URLEncoder.encode(message,'UTF 8')
 	
 	def telegramUrl = "https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${encodedMessage}"
 	sh "curl -s -x POST ${telegramUrl}"
+}
